d3b1ecd512eb8a4b7e2fa3a29489cd8ca423d430
Malik Campbell <user.email>
2023-03-21 23:41:51 -0400

    new

---
+++ function1helloWorldLvl8.js
@@ -0,0 +1,6 @@
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Write a function "greet" that returns "hello world!"
+function greet(){
+  return "hello world!"
+}
