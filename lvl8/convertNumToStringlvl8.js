0dfc8a3ddd5d114d7690172cf5568547d1938752
Malik Campbell <user.email>
2023-03-28 23:28:09 -0400

    more

---
+++ lvl8/convertNumToStringlvl8.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+function numberToString(num) {
+  // Return a string of the number here!
+  return "" + num + ""
+}
