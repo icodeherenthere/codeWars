a5d8087d80eda01a48ac50934faeed4c9f2c66df
Malik Campbell-Greene <malikcampbellgreene@gmail.com>
2023-04-06 21:58:24 -0400

    more

---
+++ lvl8/friendOrFoeLvl8.js
@@ -0,0 +1,13 @@
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// i.e.
+
+function friend(friends){
+  //your code here
+  const result = friends.filter(friends => friends.length === 4);
+  return result
+}
