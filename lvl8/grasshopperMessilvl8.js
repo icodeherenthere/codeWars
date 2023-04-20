fe27b1c66ee5acf8aaf156a69e99c06f54699481
Malik Campbell-Greene <malikcampbellgreene@gmail.com>
2023-04-19 23:15:32 -0400

    more

---
+++ lvl8/grasshopperMessilvl8.js
@@ -0,0 +1,15 @@
+// Messi goals function
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  let goalTotal = laLigaGoals + copaDelReyGoals + championsLeagueGoals 
+  return goalTotal 
+}
+goals (2,3,4)
