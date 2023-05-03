6e632636d9d9a583b70db6beee306ced1e22014e
Malik Campbell-Greene <malikcampbellgreene@gmail.com>
2023-05-02 23:30:31 -0400

    more

---
+++ lvl7/ninjaVsSamuraiLvl7.js
@@ -0,0 +1,26 @@
+// DESCRIPTION:
+// Something is wrong with our Warrior class. The strike method does not work correctly. The following shows an example of this code being used:
+
+// var ninja = new Warrior('Ninja');
+// var samurai = new Warrior('Samurai');
+
+// samurai.strike(ninja, 3);
+// // ninja.health should == 70
+// Can you figure out what is wrong?
+
+
+var Warrior = function(name){
+  this.name = name;  
+  this.health = 100;
+};
+
+Warrior.prototype.strike = function(enemy, swings){
+  enemy.health = Math.max(0, enemy.health - (swings * 10));   
+};
+
+var warrior1 = new Warrior("Bob");
+var warrior2 = new Warrior("Alice");
+
+warrior1.strike(warrior2, 2);
+
+console.log(warrior2.health); // Outputs 80
