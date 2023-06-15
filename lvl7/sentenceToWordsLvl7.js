cbcc376dc18a16001d6e9492eae005fb6c4e3011
Malik Campbell-Greene <malikcampbellgreene@gmail.com>
2023-06-15 11:10:33 -0400

    more

---
+++ lvl7/sentenceToWordsLvl7.js
@@ -0,0 +1,8 @@
+// Write function splitSentence which will create a list of strings from a string.
+
+
+function splitSentence(s) {
+  let result = s.split(' ')
+  console.log(result)
+  return result
+}
