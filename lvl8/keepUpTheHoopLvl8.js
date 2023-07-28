edcabe9722234d1ac7ef59ef3bd136ca4b4c5377
Malik Campbell-Greene <malikcampbellgreene@gmail.com>
2023-07-27 22:56:12 -0400

    more

---
+++ lvl8/keepUpTheHoopLvl8.js
@@ -0,0 +1,16 @@
+// Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+
+// If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+// If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+
+
+
+function hoopCount (n) {
+  //your code goes here  
+ console.log(n)
+ if(n <= 10){
+   return "Keep at it until you get it"
+ }else{
+   return "Great, now move on to tricks"
+ }
+}
