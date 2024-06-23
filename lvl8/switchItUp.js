bda8abd4d100e0c2d2467dd8902cff7285946921
Malik Campbell-Greene <126094295+icodeherenthere@users.noreply.github.com>
2024-06-23 19:26:46 -0400

    more

---
+++ lvl8/switchItUp.js
@@ -0,0 +1,12 @@
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+function switchItUp(number){
+    //Write your own Code!
+      let transform = ['Zero', 'One', 'Two', 'Three', 'Four', 
+            'Five', 'Six', 'Seven', 'Eight', 'Nine']
+      return transform[number]
+    }
